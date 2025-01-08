6810021ef1c958783ae475edafebcd3d8345648e
Matias <126021442+Matias-87@users.noreply.github.com>
2025-01-08 15:48:10 -0300

    feat(all project): configuration and tests of firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,39 @@
-import { Component } from '@angular/core';
+import { AsyncPipe } from '@angular/common';
+import { Component, inject, OnInit } from '@angular/core';
+import { addDoc, collection, collectionData, Firestore } from '@angular/fire/firestore';
 import { RouterOutlet } from '@angular/router';
+import { Interface } from 'readline';
+import { Observable } from 'rxjs';
+
+const PATH = 'totals';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [AsyncPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'mi-tienda-nv';
+export class AppComponent  {
+  private firestore: Firestore = inject(Firestore);
+  private totalsCollection = collection(this.firestore, PATH);
+
+  getTotals() {
+    return collectionData(this.totalsCollection) as Observable<Totals[]>;
+  }
+
+  totals$ = this.getTotals();
+
+  async addTotals(value: number) {
+    try {
+      await addDoc(this.totalsCollection, {total: value});
+    } catch (error) {
+      console.error(error);
+    }
+  }
+}
+
+export interface Totals {
+  total: number;
+  id: string;
 }
